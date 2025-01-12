1929ae0ce917a94e28d903ad6fb271e3cd7509df
Frank Utibe Joseph <majesticfrank7@gmail.com>
2025-01-12 03:03:04 +0100

    updated vite.config.js file

--- MovieApp/vite.config.js
+++ MovieApp/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure the base path is correct for your app
+  base: '/MovieApp/', // Ensure the base path is correct for your app
   build: {
     outDir: 'dist', // Ensure output directory is correct
   },
